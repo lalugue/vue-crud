bc57c370f6ae2e146ed6585dce2293103e892b7d
lalugue <51893731+lalugue@users.noreply.github.com>
2020-07-25 01:02:02 +0800

    add bootstrap, api routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+const API_GET = "http://localhost:4000/todos"
+const API_UPDATE = "http://localhost:4000/todos/update"
+const API_CREATE = "http://localhost:4000/todos/add"
 
 new Vue({
   render: h => h(App),
