312d81a6b521242f60b11fd1a0d87345eacca95f
lalugue <51893731+lalugue@users.noreply.github.com>
2020-07-27 23:45:14 +0800

    create edit form

--- src/components/EditForm.vue
+++ src/components/EditForm.vue
@@ -1,18 +1,45 @@
 <template>  
-  <p>Test</p>
+  <form>
+  <div class="form-group">    
+    <label>Todo Description</label>
+    <input id="todoInput" class="form-control" v-bind:placeholder="todo_description">   
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Owner</label>
+    <input id="ownerInput" class="form-control" :placeholder="todo_responsible">
+  </div>
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    {{todo_priority}}
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="#">Low</a>
+      <a class="dropdown-item" href="#">Medium</a>
+      <a class="dropdown-item" href="#">High</a>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 </template>
 
 <script>
 export default {
-  /*props: [
-    'todo'
-  ],*/
+  data: function(){
+    return{
+      test: "hello",
+      todo_description: this.$route.params.todo_description,
+      todo_responsible: this.$route.params.todo_responsible,
+      todo_priority: this.$route.params.todo_priority
+    }
+  },  
   mounted(){
     console.log("EditForm is mounted")
     console.log(this.$route.params)
     console.log(this)
   }
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
